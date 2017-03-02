bdfa6ff32fb2a3968723d31d5e6da4dcf44319b6
Schno Mozingo <schno.mozingo@gmail.com>
2017-03-02 13:28:19 -0800

    commiting for merge take 2

--- webpack.config.js
+++ webpack.config.js
@@ -19,12 +19,12 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         exclude: /node_modules/
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file',
+        loader: 'file-loader',
         options: {
           name: '[name].[ext]?[hash]'
         }
